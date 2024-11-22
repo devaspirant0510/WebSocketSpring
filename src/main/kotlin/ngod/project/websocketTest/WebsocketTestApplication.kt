53d83a3b48d1348b12f6dc720a1d6ebbaf7bac20
devaspirant0510 <seungho020510@gmail.com>
2024-11-22 17:42:17 +0900

    feat : 프로젝트 세팅

---
+++ src/main/kotlin/ngod/project/websocketTest/WebsocketTestApplication.kt
@@ -0,0 +1,11 @@
+package ngod.project.websocketTest
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WebsocketTestApplication
+
+fun main(args: Array<String>) {
+	runApplication<WebsocketTestApplication>(*args)
+}
---
+++ src/test/kotlin/ngod/project/websocketTest/WebsocketTestApplicationTests.kt
@@ -0,0 +1,13 @@
+package ngod.project.websocketTest
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class WebsocketTestApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
